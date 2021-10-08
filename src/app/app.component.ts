af22e489f50ea978f7d5bd9e149f6a499b319acc
John Doe <brad840688@gmail.com>
2021-10-08 09:15:00 +0800

    計算機測試修改.1008.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,97 +9,82 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   result = '';
+  nums:string[]= [];
+  num = '';
+  operators:string[]= [];
 
+  //更新顯示數字
+  renew(){
+    this.result='';
+    if(this.nums.length>0){
+    for(let i=0;i<this.nums.length;i++){
+      this.result+=(this.nums[i]+this.operators[i]);
+    }
+  }
+    this.result+=this.num;
+  }
+  // 數字功能
   addN(value: number) {
     value.toString;
-    this.result += value;
+    this.num += value;
+    this.renew();
   }
-  float() {
-    if (this.result.indexOf('.') == -1) {
-      this.result += '.';
-    } else if (this.result.charAt(this.result.length - 1) !== '.') {
-      let s = this.result.split('.');
-      if (parseInt(s[1]).toString.length !== s[1].length && s[2] === undefined) {
-        this.result += '.';
-      }
-    }
+  //小數點功能
+  dot() {
+    if (this.num.indexOf('.') == -1) {
+      this.num += '.';
+      this.renew();
+    } 
   }
-  calculate(value: string) {
-
+  // 運算子功能
+  operator(value: string){
+    this.nums[this.nums.length] = this.num ;
+    this.num = '';
+    
     switch (value) {
       case '+':
-        this.checkResult();
-        if (this.result.indexOf('+') == -1) {
-          this.result += '+';
-        }
+        this.operators[this.operators.length]='+';
+        this.renew();
         break;
       case '-':
-        this.checkResult();
-        if (this.result.indexOf('-') == -1) {
-          this.result += '-';
-        }
+        this.operators[this.operators.length]='-';
+        this.renew();
         break;
       case '*':
-        this.checkResult();
-        if (this.result.indexOf('*') == -1) {
-          this.result += '*';
-        }
+        this.operators[this.operators.length]='*';
+        this.renew();
         break;
       case '/':
-        this.checkResult();
-        if (this.result.indexOf('/') == -1) {
-          this.result += '/';
-        }
+        this.operators[this.operators.length]='/';
+        this.renew();
         break;
     }
-
-
   }
+  //清除所有數據
   clearN() {
-    this.result = '';
+   this.result = '';
+   this.nums= [];
+   this.num = '';
+   this.operators= [];
   }
-  checkResult() {
-    let s = this.result.charAt(this.result.length - 1);
-    if (isNaN(parseFloat(s))) {
-      this.result = this.result.slice(0, -1);
-    }
-    let num = this.result.split('*');
-    let x, y, z;
-
-    if (num[1] !== undefined) {
-      x = parseFloat(num[0]);
-      y = parseFloat(num[1]);
-      z = x * y;
-      this.result = z.toString()
-    } else {
-      num = this.result.split('/');
-      if (num[1] !== undefined) {
-        x = parseFloat(num[0]);
-        y = parseFloat(num[1]);
-        z = x / y;
-        this.result = z.toString()
-      } else {
-        num = this.result.split('+');
-        if (num[1] !== undefined) {
-          x = parseFloat(num[0]);
-          y = parseFloat(num[1]);
-          z = x + y;
-          this.result = z.toString()
-        } else {
-          num = this.result.split('-');
-          if (num[1] !== undefined) {
-            x = parseFloat(num[0]);
-            y = parseFloat(num[1]);
-            z = x - y;
-            this.result = z.toString()
-          }
-        }
-
+  //計算結果
+  checkResult(){
+    this.nums[this.nums.length] = this.num ;
+    this.num = '';
+    for(let i=0;i<this.operators.length;i++){
+      if(this.operators[i]=='*'){
+        this.nums[i]= (parseFloat(this.nums[i])*parseFloat(this.nums[i+1])).toString(); 
+        this.nums.splice(i+1,1)
+        console.log(this.nums);
+        
+             
+      }else if(this.operators[i]=='/'){
+        this.nums[i]= (parseFloat(this.nums[i])*parseFloat(this.nums[i+1])).toString();
+        
+        
       }
     }
-
-    // calculate() {
-    // this.result = this.x + this.y;
-    // }
   }
+ 
+
 }
